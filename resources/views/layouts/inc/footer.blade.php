973668e49294324598f767111eaf2ea2a84a6d49
malith94batugedara <malithbatugedara@gmail.com>
2024-05-20 16:29:53 +0530

    footer design done

---
+++ resources/views/layouts/inc/footer.blade.php
@@ -0,0 +1,24 @@
+<footer class="container" style="margin-left: 120px">
+   
+   <div class="row">
+         <div class="col-md-4">
+               <h1 style="color: red">Quick Links</h1><br/>
+               <a href="{{ route('players.all') }}" class="text text-warning">Players</a><br/><br/>
+               <a href="{{ route('grounds.all') }}" class="text text-warning">Grounds</a><br/><br/>
+               <a href="{{ route('schedules.all') }}" class="text text-warning">Schedules</a>
+         </div>
+         <div class="col-md-4">
+            <h1 style="color: red">Social Links</h1><br/>
+            <a href="" class="text text-warning"><i class="bi bi-facebook text-warning"></i>&nbsp Facebook</a><br/><br/>
+            <a href="" class="text text-warning"><i class="bi bi-instagram text-warning"></i>&nbsp Instagram</a><br/><br/>
+            <a href="" class="text text-warning"><i class="bi bi-youtube text-warning"></i>&nbsp Youtube</a>
+         </div>
+         <div class="col-md-4">
+            <h1 style="color: red">Contact</h1><br/>
+            <a href="" class="text text-warning">Tel : 011 2568963</a><br/><br/>
+            <a href="" class="text text-warning">Email : srilankacricket@gmail.com</a><br/><br/>
+            <a href="" class="text text-warning"><i class="bi bi-whatsapp text-warning"></i>&nbsp 071 6953625</a>
+         </div>
+   </div>
+    
+</footer>
